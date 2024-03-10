aaf7d2da06cb4c1bc28ea01d30a987c91a9650e7
AVKotov <avkotov@dev.vtb.ru>
2024-03-10 16:56:34 +0300

    Практика Java. Классы и методы. #3

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task2();
+        task3();
     }
 
     //задание 1
@@ -17,11 +17,11 @@ public class Main {
 
     public static double fraction(double x) {
         //получим целую часть
-        double integerPartOfNumber= Math.floor(x);
+        double integerPartOfNumber = Math.floor(x);
         //получим дробную часть
-        double fraction =x-integerPartOfNumber;
+        double fraction = x - integerPartOfNumber;
         //круглим до 3 знаков после запятой
-        return Math.floor( fraction * 1000) / 1000;
+        return Math.floor(fraction * 1000) / 1000;
     }
 
     //задание 2
@@ -31,11 +31,24 @@ public class Main {
         System.out.printf("результат сложения двух последних знаков числа %d." +
                 "\nРезультат : %d", x, sumLastNums(x));
     }
-    public static int sumLastNums(int x){
+
+    public static int sumLastNums(int x) {
         //получим последнее число
-        int a=x%10;
+        int a = x % 10;
         //получим предпоследнее число
-        int b=x%100/10;
-        return a+b;
+        int b = x % 100 / 10;
+        return a + b;
+    }
+
+    //задание 3
+    public static void task3() {
+        System.out.println("Введите число и нажмите <Enter>:");
+        String x = new Scanner(System.in).next();
+        System.out.printf("Получим преобразование цифры в соответствующее число из таблицы ASCII." +
+                "\nРезультат : %d", charToNum(x.charAt(0)));
+    }
+
+    public static int charToNum(char x) {
+        return (int) x;
     }
 }
