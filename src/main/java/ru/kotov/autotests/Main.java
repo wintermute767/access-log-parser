2f1c1edca66df32c85552ab60f545af283203759
AVKotov <avkotov@dev.vtb.ru>
2024-03-10 19:59:57 +0300

    Практика Java. Условия #2

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task2();
     }
 
     //задание 1
@@ -20,6 +20,23 @@ public class Main {
             return x * -1;
         }
         return x;
+    }
+    //задание 2
+    public static void task2() {
+        System.out.println("Введите число и нажмите <Enter>:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите число и нажмите <Enter>:");
+        int y = new Scanner(System.in).nextInt();
+        System.out.printf("Получим результат деление %d на %d" +
+                "\nРезультат : %d", x,y, safeDiv(x,y));
+    }
+
+    public static int safeDiv(int x, int y) {
+        Integer tempY = y;
+        if (tempY.equals(0)) {
+            return 0;
+        }
+        return x/y;
     }
 
 }
