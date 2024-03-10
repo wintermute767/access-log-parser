022f61cb2d950dae4b6fbe448b1bb8bec5cbbf65
AVKotov <avkotov@dev.vtb.ru>
2024-03-10 23:59:34 +0300

    Практика Java. Условия #5

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task4();
+        task5();
     }
 
     //задание 1
@@ -21,6 +21,7 @@ public class Main {
         }
         return x;
     }
+
     //задание 2
     public static void task2() {
         System.out.println("Введите число и нажмите <Enter>:");
@@ -28,7 +29,7 @@ public class Main {
         System.out.println("Введите число и нажмите <Enter>:");
         int y = new Scanner(System.in).nextInt();
         System.out.printf("Получим результат деление %d на %d" +
-                "\nРезультат : %d", x,y, safeDiv(x,y));
+                "\nРезультат : %d", x, y, safeDiv(x, y));
     }
 
     public static int safeDiv(int x, int y) {
@@ -36,8 +37,9 @@ public class Main {
         if (tempY.equals(0)) {
             return 0;
         }
-        return x/y;
+        return x / y;
     }
+
     //задание 3
     public static void task3() {
         System.out.println("Введите число и нажмите <Enter>:");
@@ -45,11 +47,11 @@ public class Main {
         System.out.println("Введите число и нажмите <Enter>:");
         int y = new Scanner(System.in).nextInt();
         System.out.printf("Получим максимальное значение из двух чисел: %d на %d" +
-                "\nРезультат : %d", x,y, max(x,y));
+                "\nРезультат : %d", x, y, max(x, y));
     }
 
     public static int max(int x, int y) {
-        if (((Integer)x).intValue()>((Integer)y).intValue()){
+        if (((Integer) x).intValue() > ((Integer) y).intValue()) {
             return x;
         }
         return y;
@@ -62,15 +64,40 @@ public class Main {
         System.out.println("Введите число и нажмите <Enter>:");
         int y = new Scanner(System.in).nextInt();
         System.out.printf("Получим знак операции сравнения двух чисел: %d на %d" +
-                "\nРезультат : %d %s %d", x,y,x, makeDecision(x,y),y);
+                "\nРезультат : %d %s %d", x, y, x, makeDecision(x, y), y);
     }
 
     public static String makeDecision(int x, int y) {
-        if (((Integer)x).intValue()>((Integer)y).intValue()){
+        if (((Integer) x).intValue() > ((Integer) y).intValue()) {
             return ">";
-        } else if (((Integer)x).intValue()<((Integer)y).intValue()) {
+        } else if (((Integer) x).intValue() < ((Integer) y).intValue()) {
             return "<";
         }
         return "=";
+    }
+
+    //задание 5
+    public static void task5() {
+        System.out.println("Введите число и нажмите <Enter>:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите число и нажмите <Enter>:");
+        int y = new Scanner(System.in).nextInt();
+        System.out.println("Введите число и нажмите <Enter>:");
+        int z = new Scanner(System.in).nextInt();
+        System.out.printf("Получим максимальное из трех чисел: %d, %d, %d" +
+                "\nРезультат : %d ", x, y, z, max3(x, y, z));
+    }
+
+    public static int max3(int x, int y, int z) {
+        int maxValue;
+        if (((Integer) x).intValue() > ((Integer) y).intValue()) {
+            maxValue = x;
+        } else {
+            maxValue = y;
+        }
+        if (((Integer) maxValue).intValue() < ((Integer) z).intValue()) {
+            maxValue = z;
+        }
+        return maxValue;
     }
 }
