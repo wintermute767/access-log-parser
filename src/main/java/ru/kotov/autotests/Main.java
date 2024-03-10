f3c7a74f883d036c9e74d8f6a3d299866bd81b33
AVKotov <avkotov@dev.vtb.ru>
2024-03-10 15:35:36 +0300

    Простые математические операции

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        System.out.println("Введите первое число и нажмите <Enter>: ");
+        Integer a = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите <Enter>: ");
+        Integer b = new Scanner(System.in).nextInt();
+        System.out.printf("Сложение: %d + %d = %d\n",a,b,addition(a,b));
+        System.out.printf("Вычитание: %d - %d = %d\n",a,b,subtraction(a,b));
+        System.out.printf("Умножение: %d * %d = %d\n",a,b,multiplication(a,b));
+        System.out.printf("Деление: %d : %d = %f\n",a,b,division(a,b));
     }
+    private static Integer addition(Integer a, Integer b){
+        return a+b;
+    }
+    private static Integer subtraction(Integer a, Integer b){
+        return a-b;
+    }
+    private static Integer multiplication(Integer a, Integer b){
+        return a*b;
+    }
+    private static Double division(Integer a, Integer b){
+        return (double) a/b;
+    }
+
 }
