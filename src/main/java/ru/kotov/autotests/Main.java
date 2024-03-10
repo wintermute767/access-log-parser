0cc4efe6fda272038e3e1a2a4b6300eb618c7b6f
AVKotov <avkotov@dev.vtb.ru>
2024-03-10 16:22:47 +0300

    Практика Java. Классы и методы. #1

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -4,8 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        task1();
+    }
+
+    //задание 1
+    public static void task1() {
+        System.out.println("Введите число и нажмите <Enter>:");
+        double x = new Scanner(System.in).nextDouble();
+        System.out.printf("Получим только дробную часть с 3 знаками после запятой от числа %f." +
+                "\nРезультат : %.3f", x, fraction(x));
+    }
+
+    public static double fraction(double x) {
+        //получим целую часть
+        double integerPartOfNumber= Math.floor(x);
+        //получим дробную часть
+        double fraction =x-integerPartOfNumber;
+        //круглим до 3 знаков после запятой
+        return Math.floor( fraction * 1000) / 1000;
     }
 }
