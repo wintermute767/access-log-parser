2141a5baf04282192e0e1d9082eb22300ddeb06c
AVKotov <avkotov@dev.vtb.ru>
2024-04-09 22:39:47 +0300

    Практика Java. Циклы #1

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -4,8 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        task1();
+    }
+
+    //задание 1
+    public static void task1() {
+        System.out.println("Введите число и нажмите <Enter>:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.printf("Выводит строку, в которой будут записаны все числа от 0 до %d (включительно)" +
+                "\nРезультат: %s", x, listNums(x));
+    }
+
+    public static String listNums(int x) {
+        if (x >= 0) {
+            String result = "";
+            for (int i = 0; i <= x; i++) {
+                result = result + " " + i;
+            }
+            return result;
+        }
+        return "значения должны быть от 0 и выше";
     }
 }
