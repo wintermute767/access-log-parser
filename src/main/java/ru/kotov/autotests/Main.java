aacd039ebb89ffd4b8110b5a75a44f97b680ff57
AVKotov <avkotov@dev.vtb.ru>
2024-04-23 16:19:05 +0300

    Добавлена проверка существование файла с логами

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -1,11 +1,34 @@
 package ru.kotov.autotests;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
 import java.util.Scanner;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        int rightLogFileName=0;
+        while (true) {
+            log.info("Введите путь к файлу с логами");
+            String path = new Scanner(System.in).nextLine();
+            rightLogFileName++;
+            if (!checksPathToFileLog(path)) {
+                log.warn("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            log.info("Путь указан верно. Это файл номер {}",rightLogFileName);
+        }
+
+    }
+
+    public static boolean checksPathToFileLog(String path) {
+        File file = new File(path);
+        boolean fileExists = file.exists();
+        boolean isDirectory = file.isDirectory();
+        if (fileExists && !isDirectory) {
+            return true;
+        }
+        return false;
     }
 }
