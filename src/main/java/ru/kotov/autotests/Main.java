9458382dd328a20f479b42f8ad5adaa60ce943ff
AVKotov <avkotov@dev.vtb.ru>
2024-04-23 16:46:45 +0300

    Практика Java. Циклы #2

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -9,13 +9,21 @@ import java.util.Scanner;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("результат: "+ findFirst(new int[]{1, 2, 3, 4, 2, 2, 5}, 3));
+        System.out.println("результат: " + findLast(new int[]{1, 2, 3, 4, 2, 2, 5}, 2));
 
     }
 
     //задание 1
     public static int findFirst(int[] arr, int x) {
         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) return i;
+        }
+        return -1;
+    }
+
+    //задание 2
+    public static int findLast(int[] arr, int x) {
+        for (int i = arr.length-1; i >= 0; i--) {
             if (arr[i] == x) return i;
         }
         return -1;
