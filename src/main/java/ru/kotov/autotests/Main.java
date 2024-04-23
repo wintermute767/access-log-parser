2426d35e9c32d7463a22464334df21131c61393a
AVKotov <avkotov@dev.vtb.ru>
2024-04-23 16:40:15 +0300

    Практика Java. Циклы #1

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -3,32 +3,21 @@ package ru.kotov.autotests;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.util.List;
 import java.util.Scanner;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        int rightLogFileName=0;
-        while (true) {
-            log.info("Введите путь к файлу с логами");
-            String path = new Scanner(System.in).nextLine();
-            rightLogFileName++;
-            if (!checksPathToFileLog(path)) {
-                log.warn("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
-                continue;
-            }
-            log.info("Путь указан верно. Это файл номер {}",rightLogFileName);
-        }
+        System.out.println("результат: "+ findFirst(new int[]{1, 2, 3, 4, 2, 2, 5}, 3));
 
     }
 
-    public static boolean checksPathToFileLog(String path) {
-        File file = new File(path);
-        boolean fileExists = file.exists();
-        boolean isDirectory = file.isDirectory();
-        if (fileExists && !isDirectory) {
-            return true;
+    //задание 1
+    public static int findFirst(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) return i;
         }
-        return false;
+        return -1;
     }
 }
