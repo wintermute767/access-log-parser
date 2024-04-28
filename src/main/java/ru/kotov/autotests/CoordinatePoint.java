5a2bcd72b5eb26b75a3930c2cef89e1f46610e65
AVKotov <avkotov@dev.vtb.ru>
2024-04-28 19:43:30 +0300

    Практика Java. Объекты и классы #1

---
+++ src/main/java/ru/kotov/autotests/CoordinatePoint.java
@@ -0,0 +1,35 @@
+package ru.kotov.autotests;
+
+import java.util.Objects;
+
+public class CoordinatePoint {
+
+    private int coordinateX;
+    private int coordinateY;
+
+
+    public CoordinatePoint(int coordinateX, int coordinateY) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+    @Override
+    public String toString() {
+        return "CoordinatePoint{" +
+                "coordinateX=" + coordinateX +
+                ", coordinateY=" + coordinateY +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CoordinatePoint that)) return false;
+        return coordinateX == that.coordinateX && coordinateY == that.coordinateY;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinateX, coordinateY);
+    }
+}
--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -5,30 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.util.Scanner;
 
-@Slf4j
+
 public class Main {
     public static void main(String[] args) {
-        int rightLogFileName=0;
-        while (true) {
-            log.info("Введите путь к файлу с логами");
-            String path = new Scanner(System.in).nextLine();
-            rightLogFileName++;
-            if (!checksPathToFileLog(path)) {
-                log.warn("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
-                continue;
-            }
-            log.info("Путь указан верно. Это файл номер {}",rightLogFileName);
-        }
+        CoordinatePoint point1 = new CoordinatePoint(1, 3);
+        CoordinatePoint point2 = new CoordinatePoint(1, 3);
+        CoordinatePoint point3 = new CoordinatePoint(5, 8);
 
-    }
-
-    public static boolean checksPathToFileLog(String path) {
-        File file = new File(path);
-        boolean fileExists = file.exists();
-        boolean isDirectory = file.isDirectory();
-        if (fileExists && !isDirectory) {
-            return true;
-        }
-        return false;
+        System.out.println("Сравнение точек " + point1.toString() + " и " + point2.toString() + ": " + (point1 == point2));
+        System.out.println("Сравнение точек " + point2.toString() + " и " + point3.toString() + ": " + (point2 == point3));
+        System.out.println("Сравнение точек " + point1.toString() + " и " + point3.toString() + ": " + (point1 == point3));
     }
 }
