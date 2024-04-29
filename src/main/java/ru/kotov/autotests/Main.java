8f10ab0633e7e34e0427207d5a9357e03076b0b5
AVKotov <avkotov@dev.vtb.ru>
2024-04-29 21:58:56 +0300

    Практика Java.

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -1,23 +1,18 @@
 package ru.kotov.autotests;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        PolyLine polyLine = new PolyLine();
-        polyLine.setArrayCoordinatePoint(new CoordinatePoint[]{
-                new CoordinatePoint(1, 5),
-                new CoordinatePoint(2, 8),
-                new CoordinatePoint(5, 3),
-                new CoordinatePoint(8, 9)});
-        ClosedPolyLine closedPolyLine = new ClosedPolyLine();
-        closedPolyLine.setArrayCoordinatePoint(new CoordinatePoint[]{
-                new CoordinatePoint(1, 5),
-                new CoordinatePoint(2, 8),
-                new CoordinatePoint(5, 3),
-                new CoordinatePoint(8, 9)});
-        System.out.println("Длина polyLine: "+ polyLine.getLength(polyLine.getArrayCoordinatePoint()));
-        System.out.println("Длина closedPolyLine: "+ closedPolyLine.getLength(closedPolyLine.getArrayCoordinatePoint()));
+
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 5);
+        Fraction f3 = new Fraction(7, 8);
+        System.out.println(f1.sum(f2).sum(f3).minus(5));
     }
+
 }
