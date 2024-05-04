3e788f7f4d7650863ed31a25a6731db4ba979268
AVKotov <avkotov@dev.vtb.ru>
2024-05-04 16:12:36 +0300

    Практика Java. Исключения #2

--- src/main/java/ru/сourses/main/Sum.java
+++ src/main/java/ru/сourses/main/Sum.java
@@ -1,4 +1,6 @@
+package ru.сourses.main;
 
+import java.util.Arrays;
 
 public class Sum {
     public static void main(String[] args) {
@@ -6,14 +8,45 @@ public class Sum {
 
         for (String arg : args) {
             double argDouble;
-            try {
+            if (checkingArg(arg)) {
                 argDouble = Double.parseDouble(arg);
-            } catch (NumberFormatException e) {
-                System.out.println("Аргумент " + arg + " не является числом. Ошибка: " + e.getMessage());
+            } else {
                 argDouble = 0;
             }
-            result+=argDouble;
+            result += argDouble;
         }
-        System.out.println("результат: "+result);
+        System.out.println("результат: " + result);
+    }
+
+    private static boolean checkingArg(String arg) {
+        //проверим каждый аргумент
+        //список символов после запятой
+        String afterPointСorrectSymbols = "0123456789";
+        //список символов до запятой
+        String beforePointСorrectSymbols = afterPointСorrectSymbols + "-";
+        //разделим на до и после запятой
+        String[] argArray;
+        if (arg.contains(".")) {
+            argArray = arg.split("\\.");
+        } else if (arg.contains(",")) {
+            argArray = arg.split(",");
+        } else argArray = new String[]{arg};
+
+        if (argArray.length == 1) {
+            //если запятой нет то проверим только один раз
+            return checkingString(argArray[0], beforePointСorrectSymbols);
+        } else if (argArray.length == 2) {
+            //если запятая есть то проверим обе части
+            return checkingString(argArray[0], beforePointСorrectSymbols) && checkingString(argArray[1], afterPointСorrectSymbols);
+        } else
+            //если 2 и более запятых то сразу false
+            return false;
+    }
+
+    private static boolean checkingString(String stingForChecking, String stringWithСorrectSymbols) {
+        return Arrays.stream(stingForChecking.split(""))
+                .filter(s -> !stringWithСorrectSymbols.contains(s))
+                .findFirst()
+                .isEmpty();
     }
 }
