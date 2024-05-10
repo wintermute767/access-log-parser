16aea21e95569a558bc1676c4f893eb302cad960
AVKotov <avkotov@dev.vtb.ru>
2024-05-10 20:47:33 +0300

    Практика Java. Коллекции №2

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -14,14 +14,36 @@ import static ru.kotov.autotests.logerReader.LogReader.checksPathToFileLog;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        List<Integer> simpleList = newListWithSetSize(12);
-        System.out.println("До перестановки: ");
-        simpleList.forEach(integer -> System.out.print(integer+" "));
-        pairwisePermutation(simpleList);
-        System.out.println("\nПосле перестановки: ");
-        simpleList.forEach(integer -> System.out.print(integer+" "));
-    }
+        ArrayList<Integer> listForTest = new ArrayList<>(Arrays.asList(45,123,34,65,77,1,343));
+        int[] arrayForTest = new int[]{45,123,34,65,77,1,343};
+        bubbleSort(listForTest);
+        bubbleSort(arrayForTest);
+        System.out.println("Сортировка листа: "+ listForTest);
+        System.out.println("Сортировка массива: "+Arrays.toString(arrayForTest));
 
+    }
+    public static void bubbleSort(int[] intArray) {
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = 0; j < intArray.length  - i - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j];
+                    intArray[j]=intArray[j + 1];
+                    intArray[j + 1]=temp;
+                }
+            }
+        }
+    }
+    public static void bubbleSort(ArrayList<Integer> intList) {
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
+            }
+        }
+    }
     public static List<Integer> newListWithSetSize(int size) {
         List<Integer> simpleList = new ArrayList<>();
         for (int i = 1; i <= size; i++) {
