29841d62a8c0f2d1e5d57017a0cd6f40f9e37c2e
AVKotov <avkotov@dev.vtb.ru>
2024-05-10 19:31:47 +0300

    Практика Java. Коллекции №1

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -3,6 +3,9 @@ package ru.kotov.autotests;
 import lombok.extern.slf4j.Slf4j;
 import ru.kotov.autotests.logerReader.LogReader;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static ru.kotov.autotests.logerReader.LogReader.checksPathToFileLog;
@@ -11,20 +14,26 @@ import static ru.kotov.autotests.logerReader.LogReader.checksPathToFileLog;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        int rightLogFileName = 0;
-        while (true) {
-            log.info("Введите путь к файлу с логами");
-            String path = new Scanner(System.in).nextLine();
-            rightLogFileName++;
-            if (!checksPathToFileLog(path)) {
-                log.warn("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
-                continue;
-            }
-            log.info("Путь указан верно. Это файл номер {}", rightLogFileName);
-            LogReader logReader = new LogReader();
-            logReader.readLogFile(path);
-            log.info(logReader.getLogStatistic().toString());
-        }
+        List<Integer> simpleList = newListWithSetSize(12);
+        System.out.println("До перестановки: ");
+        simpleList.forEach(integer -> System.out.print(integer+" "));
+        pairwisePermutation(simpleList);
+        System.out.println("\nПосле перестановки: ");
+        simpleList.forEach(integer -> System.out.print(integer+" "));
     }
 
+    public static List<Integer> newListWithSetSize(int size) {
+        List<Integer> simpleList = new ArrayList<>();
+        for (int i = 1; i <= size; i++) {
+            simpleList.add(i);
+        }
+        return simpleList;
+    }
+
+    public static void pairwisePermutation(List<Integer> listForPermutation) {
+        int firstElement = listForPermutation.getFirst();
+        int sizeList = listForPermutation.size();
+        listForPermutation.removeFirst();
+        listForPermutation.add(firstElement);
+    }
 }
