ec2333aa7dd6661abae01a4db59574115dede625
AVKotov <avkotov@dev.vtb.ru>
2024-05-10 21:01:34 +0300

    Практика Java. Коллекции №3

--- src/main/java/ru/kotov/autotests/Main.java
+++ src/main/java/ru/kotov/autotests/Main.java
@@ -14,25 +14,46 @@ import static ru.kotov.autotests.logerReader.LogReader.checksPathToFileLog;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> listForTest = new ArrayList<>(Arrays.asList(45,123,34,65,77,1,343));
-        int[] arrayForTest = new int[]{45,123,34,65,77,1,343};
-        bubbleSort(listForTest);
-        bubbleSort(arrayForTest);
-        System.out.println("Сортировка листа: "+ listForTest);
-        System.out.println("Сортировка массива: "+Arrays.toString(arrayForTest));
+        ArrayList<Integer> listForTest = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        int[] arrayForTest = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        reverse(listForTest);
+        reverse(arrayForTest);
+        System.out.println("Сортировка листа: " + listForTest);
+        System.out.println("Сортировка массива: " + Arrays.toString(arrayForTest));
 
     }
+
+    public static void reverse(ArrayList<Integer> intList) {
+        int n = intList.size() - 1;
+        for (int i = 0; i < intList.size() / 2; i++) {
+            int temp = intList.get(i);
+            intList.set(i, intList.get(n-i));
+            intList.set(n - i, temp);
+        }
+    }
+
+    public static void reverse(int[] intArray) {
+        int n = intArray.length - 1;
+        for (int i = 0; i < intArray.length / 2; i++) {
+            int temp = intArray[i];
+            intArray[i] = intArray[n - i];
+            intArray[n - i] = temp;
+        }
+    }
+
+
     public static void bubbleSort(int[] intArray) {
         for (int i = 0; i < intArray.length - 1; i++) {
-            for (int j = 0; j < intArray.length  - i - 1; j++) {
+            for (int j = 0; j < intArray.length - i - 1; j++) {
                 if (intArray[j] > intArray[j + 1]) {
                     int temp = intArray[j];
-                    intArray[j]=intArray[j + 1];
-                    intArray[j + 1]=temp;
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
                 }
             }
         }
     }
+
     public static void bubbleSort(ArrayList<Integer> intList) {
         for (int i = 0; i < intList.size() - 1; i++) {
             for (int j = 0; j < intList.size() - i - 1; j++) {
@@ -44,6 +65,7 @@ public class Main {
             }
         }
     }
+
     public static List<Integer> newListWithSetSize(int size) {
         List<Integer> simpleList = new ArrayList<>();
         for (int i = 1; i <= size; i++) {
