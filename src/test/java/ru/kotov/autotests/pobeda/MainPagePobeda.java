d963f70bb614cb2944e73cfeedcc8f9d7b0ab530
AVKotov <avkotov@dev.vtb.ru>
2024-06-01 15:03:50 +0300

    Практика Java. Page Object #1 - версия с FindBy

--- src/test/java/ru/kotov/autotests/pobeda/MainPagePobeda.java
+++ src/test/java/ru/kotov/autotests/pobeda/MainPagePobeda.java
@@ -1,9 +1,12 @@
 package ru.kotov.autotests.pobeda;
 
+import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,21 +14,30 @@ import java.util.concurrent.TimeUnit;
 
 public class MainPagePobeda {
 
-    WebDriver webDriver;
+    private WebDriver webDriver;
 
-    By logotypeImg = By.cssSelector("#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > a:first-child > img");
+    //    By logotypeImg = By.cssSelector();
+    @FindBy(css = "#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > a:first-child > img")
+    private  WebElement logotypeImg;
 
-    By informationElement = By.cssSelector("#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-4d364c-root-root > div.dp-12p2oi2-root > a:nth-child(1)");
+    @Getter
+    @FindBy(css = "#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-4d364c-root-root > div.dp-12p2oi2-root > a:nth-child(1)")
+    private  WebElement informationElement;
 
-    By flightPreparationElement = By.cssSelector("#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-n2y4xa-root > div > div > div:nth-child(1) > a");
+    @FindBy(css = "#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-n2y4xa-root > div > div > div:nth-child(1) > a")
+    private WebElement flightPreparationElement;
 
-    By helpfulInformationElement = By.cssSelector("#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-n2y4xa-root > div > div > div:nth-child(2) > a");
+    @FindBy(css = "#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-n2y4xa-root > div > div > div:nth-child(2) > a")
+    private  WebElement helpfulInformationElement;
 
-    By informationAboutCompanyElement = By.cssSelector("#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-n2y4xa-root > div > div > div:nth-child(3) > a");
+    @FindBy(css = "#__next > div.dp-lw1vya-root > header > div.dp-rpeswh-root > div > div > div.dp-n2y4xa-root > div > div > div:nth-child(3) > a")
+    private   WebElement informationAboutCompanyElement;
+
     public MainPagePobeda(WebDriver webDriver) {
         this.webDriver = webDriver;
         this.webDriver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         this.webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        PageFactory.initElements(this.webDriver, this);
     }
 
     public String getTitleText() {
@@ -33,29 +45,27 @@ public class MainPagePobeda {
     }
 
     public WebElement getLogotypeElement() {
-        return this.webDriver.findElement(logotypeImg);
-    }
-
-    public WebElement getInformationElement() {
-        return this.webDriver.findElement(informationElement);
+        return logotypeImg;
     }
 
     public WebElement getFlightPreparationElement() {
         Actions action = new Actions(this.webDriver);
         action.moveToElement(getInformationElement());
         action.perform();
-        return this.webDriver.findElement(flightPreparationElement);
+        return flightPreparationElement;
     }
+
     public WebElement getHelpfulInformationElement() {
         Actions action = new Actions(this.webDriver);
         action.moveToElement(getInformationElement());
         action.perform();
-        return this.webDriver.findElement(helpfulInformationElement);
+        return helpfulInformationElement;
     }
+
     public WebElement getInformationAboutCompanyElement() {
         Actions action = new Actions(this.webDriver);
         action.moveToElement(getInformationElement());
         action.perform();
-        return this.webDriver.findElement(informationAboutCompanyElement);
+        return informationAboutCompanyElement;
     }
 }
